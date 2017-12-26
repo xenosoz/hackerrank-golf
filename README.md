1e82087ab290d363232ac0a9834843107af660ec
Taihyun Hwang <xenosoz.hwang@gmail.com>
2017-12-27 05:09:33 +0900

    update progress status, add script for it

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# hackerrank-golf
 
-* [HackerRank](https://www.hackerrank.com)
+# HackerRank Golf
+
+We solve [HackerRank](https://www.hackerrank.com) for some knowledge and fun!
+Contact me if you want to join us!
+
+* Taihyun Hwang &lt;xenosoz.hwang at gmail.com&gt;
+
+## Progress
+<table>
+  <thead>
+    <tr>
+      <th>challenge</th>
+      <th>xenosoz</th>
+      <th>같이 해요!</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th><a href="https://www.hackerrank.com/challenges/apple-and-orange">apple-and-orange</a></th>
+      <th>✔</th>
+      <th></th>
+    </tr>
+    <tr>
+      <th><a href="https://www.hackerrank.com/challenges/equality-in-a-array">equality-in-a-array</a></th>
+      <th>✔</th>
+      <th></th>
+    </tr>
+    <tr>
+      <th><a href="https://www.hackerrank.com/challenges/grading">grading</a></th>
+      <th>✔</th>
+      <th></th>
+    </tr>
+  </tbody>
+</table>
---
+++ bin/visualize-progress.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+'''Generate progress graph and overwrite to README.md'''
+
+import os
+import sys
+import re
+from textwrap import dedent
+from glob import glob
+
+def header():
+    x = '''
+    # HackerRank Golf
+
+    We solve [HackerRank](https://www.hackerrank.com) for some knowledge and fun!
+    Contact me if you want to join us!
+
+    * Taihyun Hwang &lt;xenosoz.hwang at gmail.com&gt;
+    '''
+    return dedent(x)
+
+def here():
+    return os.path.dirname(sys.argv[0])
+
+def hackerrank_golf_path():
+    return os.path.join(here(), '..')
+
+def domains_path():
+    return os.path.join(hackerrank_golf_path(), 'domains')
+
+def users():
+    return ['xenosoz', '같이 해요!']
+
+def filestats():
+    pattern = '\.'.join([
+        '(?P<user_id>[a-zA-Z0-9_-]+)',
+        '(?P<challenge_id>[a-zA-Z0-9_-]+)',
+        '(?P<language>[a-zA-Z0-9_-]+)',
+    ])
+    pattern = re.compile(pattern)
+
+    for filepath in glob(os.path.join(domains_path(), '**', '*.py'), recursive=True):
+        basename = os.path.basename(filepath)
+        m = re.match(pattern, basename)
+        if m:
+            yield m.groupdict()
+
+
+def chal_to_url(chal):
+    return 'https://www.hackerrank.com/challenges/{}'.format(chal)
+
+
+def chal_to_link(chal):
+    url = chal_to_url(chal)
+    return '<a href="{}">{}</a>'.format(url, chal)
+
+
+def progress_table():
+    ret = []
+    def print(s):  # XXX
+        ret.append(s)
+
+    stats = list(filestats())
+    challenges = sorted(x['challenge_id'] for x in stats)
+    oracle = set((x['user_id'], x['challenge_id']) for x in stats)
+
+    print('<table>')
+    print('  <thead>')
+    print('    <tr>')
+    print('      <th>challenge</th>')
+    for user in users():
+        print('      <th>{}</th>'.format(user))
+    print('    </tr>')
+    print('  </thead>')
+
+    print('  <tbody>')
+    for chal in challenges:
+        print('    <tr>')
+        print('      <th>{}</th>'.format(chal_to_link(chal)))
+        for user in users():
+            solved = ((user, chal) in oracle)
+            print('      <th>{}</th>'.format(['', '✔'][solved]))
+        print('    </tr>')
+    print('  </tbody>')
+    print('</table>')
+
+    return '\n'.join(ret)
+
+print(header())
+print('## Progress')
+print(progress_table())
