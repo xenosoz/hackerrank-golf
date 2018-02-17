8519262ddcb813c54b51b6f852998adbd8d81f83
kiyeon25 <daebak-kijeong@daum.net>
2018-02-17 21:14:53 +0900

    daebak.cats-and-a-mouse.python3.py

--- domains/algorithms-implementation/cats-and-a-mouse/daebak.cats-and-a-mouse.python3.py
+++ domains/algorithms-implementation/cats-and-a-mouse/daebak.cats-and-a-mouse.python3.py
@@ -33,25 +33,3 @@ if __name__ == "__main__":
         result = catAndMouse(x, y, z)
         print ("".join(map(str, result)))
 
-#태현's 코드
-
-
-
-
-##magic-square-forming
-import sys
-
-
-def formingMagicSquare(s):
-
-
-# Complete this function
-
-if __name__ == "__main__":
-    s = []
-    for s_i in range(3):
-        s_t = [int(s_temp) for s_temp in input().strip().split(' ')]
-        s.append(s_t)
-    result = formingMagicSquare(s)
-    print(result)
-
